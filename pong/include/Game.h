0ed6104d15d745632bd83e0d7d7f894fa078ec46
Your Name <youremail@example.com>
2025-12-18 18:43:38 +0700

    expand

--- pong/include/Game.h
+++ pong/include/Game.h
@@ -11,11 +11,15 @@ public:
     void run();
 
 private:
-    void handleEvents();
+    void update(float dt);
     void render();
+    void handleEvents();
     void cleanUp();
 
     bool running;
+
     SDL_Window *window;
     SDL_Renderer *renderer;
+    // what does const float do ? what does this division mean ?
+    const float targetFrameTime = 1.0f / 60.0f;
 };
--- pong/src/Game.cpp
+++ pong/src/Game.cpp
@@ -55,22 +55,60 @@ bool Game::init()
 
 void Game::run()
 {
-
+    Uint32 lastTick = SDL_GetTicks();
+    std::cout << "last Tick: " << lastTick << "\n";
     SDL_Event event;
     while (running)
     {
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-            {
-                std::cout << "end loop" << std::endl;
-                running = false;
-            }
-        }
+        Uint32 currentTick = SDL_GetTicks();
+        std::cout << "current tick: " << currentTick << "\n";
+        float deltaTime = (currentTick - lastTick) / 1000.0f;
+        std::cout << "delta time: " << deltaTime << "\n";
+        lastTick = currentTick;
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
+        update(deltaTime);
+        render();
+        handleEvents();
+    }
+}
+
+void Game::update(float dt)
+{
+    // what kind of (void)dt ? syntax , keyword, statement , or else... ? , what does "dt" stand for ? why update() not use lastTick and currentTick but run() do ? and in here not have any code to ask cpu how can frames update like the time we want ?
+    (void)dt; // silence unused warning
+    std::cout << "dt: " << dt << std::endl;
+}
+
+void Game::render()
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // what does SDL_Rect do ? how does it behave ? why does it exists ? what does x , y ,w , h mean ? how to align it ?
+    SDL_Rect paddle;
+    paddle.x = 50;
+    paddle.y = 150;
+    paddle.w = 20;
+    paddle.h = 100;
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    // why renderer don't need & , but paddle need ? what is the different between * and & ? what does SDL_RenderFillRect do ? how does it behave(or return) ?
+    SDL_RenderFillRect(renderer, &paddle);
+
+    SDL_RenderPresent(renderer);
+}
+
+void Game::handleEvents()
+{
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT)
+        {
+            std::cout << "end loop" << std::endl;
+            running = false;
+        }
     }
 }
 
