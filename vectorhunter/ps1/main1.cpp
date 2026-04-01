8e99bd889e7613034e7b4c98eb180685a2c6b450
Your Name <youremail@example.com>
2026-04-02 06:57:18 +0700

    2/4 xong bài tập đầu tiên tính canh_huyen của chương vector phần ôn luyện

--- vectorhunter/ps1/main1.cpp
+++ vectorhunter/ps1/main1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 struct Vector
 {
@@ -13,7 +14,8 @@ int main()
     vector.x = 3;
     vector.y = 5;
 
-    square_root(vector.y, vector.x);
+    float canh_huyen = square_root(vector.y, vector.x);
+    std::cout << "canh_huyen: " << canh_huyen << std::endl; // tuc la do dai cua vector
     return 0;
 }
 
@@ -24,5 +26,5 @@ float square_root(int canh_doi, int canh_ke)
     canh_huyen = (canh_doi * canh_doi) + (canh_ke * canh_ke);
     std::cout << "cạnh huyền chưa dùng căn bậc 2: " << canh_huyen << std::endl;
 
-    return canh_huyen;
+    return sqrt(canh_huyen);
 }
