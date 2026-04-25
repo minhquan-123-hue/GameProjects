c513e4ef63f5745df5391cc49ae5608aa98ee902
Your Name <youremail@example.com>
2026-04-25 19:35:36 +0700

    25/4 main pong-2 update thêm file push.lua để xử lý hình ảnh

--- cs502d/pong/main.lua
+++ cs502d/pong/main.lua
@@ -1,27 +1,40 @@
 WINDOW_WIDTH = 1280 -- constant thôi 
 WINDOW_HEIGHT = 720
 
---API love sẽ tự động nói chuyện với hệ điều hành , và yêu cầu kết nối với cách chương trình để xử lý phần âm thanh , đồ họa , font, ...
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
-function love.load() -- hàm này là hàm con của love , và nó sẽ tự động được gọi khi dùng lệnh love . trong terminal 
-    -- thiết lập trạng thái cho của sổ 
+
+push = require 'push'
+
+function love.load() 
+    love.graphics.setDefaultFilter("nearest", "nearest")
     love.window.setMode(
         WINDOW_WIDTH, WINDOW_HEIGHT,
         {
-            
         resizable = false;
         vsync = true;
         fullscreen = false;
     })
-end  
 
-function love.draw() -- hàm con 
-    -- graphics là một object trong object love , và nó chứa hàm con là graphics 
-    -- không cần hết tham số của API chỉ cần biết nó làm gì , và dùng thế nào là được
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, {upscale = 'normal'})
+end
+
+
+function love.keypressed(key)
+    if key == "espaced" then
+        love.event.quit()
+    end
+end
+
+
+function love.draw()
+    push.begin() 
     love.graphics.printf(
         "Hello, Pong!", 
         0,
-        WINDOW_HEIGHT/2 -6, 
-        WINDOW_WIDTH,
+        VIRTUAL_HEIGHT/2 -6,
+        VIRTUAL_WIDTH,
         "center")
+    push.finish()
 end
